fff1a0d3bd63e7e2216d9956918ff378d873ce7b
bensku <bmi@mbnet.fi>
2016-04-11 18:37:03 +0300

    Fix some entity/location problems

--- src/main/java/ch/njol/skript/effects/EffVisualEffect.java
+++ src/main/java/ch/njol/skript/effects/EffVisualEffect.java
@@ -52,7 +52,7 @@ import ch.njol.util.Kleenean;
 public class EffVisualEffect extends Effect {
 	static {
 		Skript.registerEffect(EffVisualEffect.class, "(play|show) %visualeffects% (on|%directions%) %entities/locations% [(to %-players%|in (radius|range) of %number%)]",
-				"(play|show) %number% %visualeffects% (on|%directions%) %entities/locations% [(to %-players%|in (radius|range) of %number%)]");
+				"(play|show) %number% %visualeffects% (on|%directions%) %locations% [(to %-players%|in (radius|range) of %number%)]");
 	}
 	
 	@SuppressWarnings("null")
