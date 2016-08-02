f41677539d92580bcb45dbaa88817355a7d43647
bensku <bmi@mbnet.fi>
2016-08-02 13:01:11 +0300

    WOOHOO! Gradle build works! (#145)
    Remove .gradle from git and add to .gitignore
    
    Replace Bukkit dependencies with Spigot API for Eclipse

--- src/main/java/ch/njol/skript/effects/Delay.java
+++ src/main/java/ch/njol/skript/effects/Delay.java
@@ -13,10 +13,10 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * 
+ *
+ *
  * Copyright 2011-2014 Peter Güttinger
- * 
+ *
  */
 
 package ch.njol.skript.effects;
@@ -54,17 +54,17 @@ public class Delay extends Effect {
 	static {
 		Skript.registerEffect(Delay.class, "(wait|halt) [for] %timespan%");
 	}
-	
+
 	@SuppressWarnings("null")
-	private Expression<Timespan> duration;
-	
+	protected Expression<Timespan> duration;
+
 	@SuppressWarnings({"unchecked", "null"})
 	@Override
 	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
 		duration = (Expression<Timespan>) exprs[0];
 		return true;
 	}
-	
+
 	@Override
 	@Nullable
 	protected TriggerItem walk(final Event e) {
@@ -87,22 +87,22 @@ public class Delay extends Effect {
 		}
 		return null;
 	}
-	
+
 	@SuppressWarnings("null")
 	protected final static Set<Event> delayed = Collections.newSetFromMap(new WeakHashMap<Event, Boolean>());
-	
+
 	public final static boolean isDelayed(final Event e) {
 		return delayed.contains(e);
 	}
-	
+
 	@Override
 	protected void execute(final Event e) {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	@Override
 	public String toString(final @Nullable Event e, final boolean debug) {
 		return "wait for " + duration.toString(e, debug) + (e == null ? "" : "...");
 	}
-	
+
 }
