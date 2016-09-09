f5dce8e446cd003ae2ea4be4dcb88f777276b6ac
Sharpjaws <terwoo2@live.nl>
2016-09-09 16:49:33 +0200

    Reverted unnecessary changes

--- src/main/java/ch/njol/skript/effects/EffPotion.java
+++ src/main/java/ch/njol/skript/effects/EffPotion.java
@@ -90,7 +90,7 @@ public class EffPotion extends Effect {
 		if (ts.length == 0)
 			return;
 		if (!apply) {
-			for (LivingEntity en : entities.getAll(e)) {
+			for (LivingEntity en : entities.getArray(e)) {
 				for (final PotionEffectType t : ts)
 					en.removePotionEffect(t);
 			}
