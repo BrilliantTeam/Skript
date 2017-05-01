cded560b362e7ff2325bf8b95d74ab24e607ec9a
bensku <bmi@mbnet.fi>
2017-05-01 13:07:31 +0300

    Improve AsyncEffect

--- src/main/java/ch/njol/skript/util/AsyncEffect.java
+++ src/main/java/ch/njol/skript/util/AsyncEffect.java
@@ -22,7 +22,10 @@ package ch.njol.skript.util;
 import ch.njol.skript.Skript;
 import ch.njol.skript.effects.Delay;
 import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Trigger;
 import ch.njol.skript.lang.TriggerItem;
+import ch.njol.skript.timings.SkriptTimings;
+
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
@@ -31,28 +34,45 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * @author Andrew Tran
+ * Effects that extend this class are ran asynchronously. Next trigger item will be ran
+ * in main server thread, as if there had been a delay before.
+ * <p>
+ * Majority of Skript and Minecraft APIs are not thread-safe, so be careful.
  */
-public abstract class AsyncEffect extends Effect{
+public abstract class AsyncEffect extends Effect {
+	
     @Override
     @Nullable
     protected TriggerItem walk(Event e) {
-        Delay.addDelayedEvent(e);
-        Bukkit.getScheduler().runTaskAsynchronously(Skript.getInstance(), new Runnable() {
-            @Override
-            public void run() {
-                execute(e);
-                Bukkit.getScheduler().runTask(Skript.getInstance(), new Runnable() {
-                    @Override
-                    public void run() {
-                        TriggerItem next = getNext();
-                        if (next != null){
-                            TriggerItem.walk(next, e);
-                        }
-                    }
-                });
-            }
-        });
+		debug(e, true);
+		TriggerItem next = getNext();
+    	
+		if (next != null) {
+	        Delay.addDelayedEvent(e);
+	        Bukkit.getScheduler().runTaskAsynchronously(Skript.getInstance(), new Runnable() {
+	            @SuppressWarnings("synthetic-access")
+				@Override
+	            public void run() {
+	                execute(e); // Execute this effect
+	                Bukkit.getScheduler().runTask(Skript.getInstance(), new Runnable() {
+	                    @Override
+	                    public void run() { // Walk to next item synchronously
+	    					Object timing = null;
+	    					if (SkriptTimings.enabled()) { // getTrigger call is not free, do it only if we must
+	    						Trigger trigger = getTrigger();
+	    						if (trigger != null) {
+	    							timing = SkriptTimings.start(trigger.getDebugLabel());
+	    						}
+	    					}
+	    					
+	    					TriggerItem.walk(next, e);
+	    					
+	    					SkriptTimings.stop(timing); // Stop timing if it was even started
+	                    }
+	                });
+	            }
+	        });
+		}
         return null;
     }
 }
