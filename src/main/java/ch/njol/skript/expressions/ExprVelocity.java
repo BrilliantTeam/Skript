5bd8aef6f923350c55ca64a6002fdd8f5edfd709
Sashie <ud13n0w@gmail.com>
2017-06-11 12:28:35 -0230

    forgot license

--- src/main/java/ch/njol/skript/expressions/ExprVelocity.java
+++ src/main/java/ch/njol/skript/expressions/ExprVelocity.java
@@ -1,3 +1,22 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Copyright 2011-2017 Peter Güttinger and contributors
+ */
 package ch.njol.skript.expressions;
 
 import org.bukkit.entity.Entity;
@@ -18,7 +37,9 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
 
-
+/**
+ * @author Sashie
+ */
 @Name("Vectors - Velocity")
 @Description("Gets, sets, adds or removes velocity to/from/of an entity")
 @Examples({"set player's velocity to {_v}"})
