7a454f4f60ba9abc54a862f55bad59f07dd62218
xXAndrew28Xx <andrewtran312@gmail.com>
2017-08-22 12:17:29 -0500

    Remove unnecessary NonNull annotation import

--- src/main/java/ch/njol/skript/variables/DatabaseStorage.java
+++ src/main/java/ch/njol/skript/variables/DatabaseStorage.java
@@ -35,7 +35,6 @@ import lib.PatPeter.SQLibrary.SQLite;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
-import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 
 import ch.njol.skript.Skript;
