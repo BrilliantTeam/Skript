ce87e67977cf6cf44d578e9cd126fb91f1c0b2aa
bensku <bmi@mbnet.fi>
2017-09-16 17:25:27 +0300

    Test nightly building

--- buildbot-compile.sh
+++ buildbot-compile.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Compiles Skript with some special settings used to produce unstable builds
 
-SKRIPT_VER="$(git rev-parse --short HEAD)-unstable"
+# Version identifier: 2.2, date YYMMDD, git head
+SKRIPT_VER="2.2-$(date +%y%m%d)-git-$(git rev-parse --short HEAD)"
 export SKRIPT_VERSION=$SKRIPT_VER
 
-./gradlew clean build
+./gradlew clean build # Clean all, then build
 
+# Write name for upload script in my server to use
 echo "$SKRIPT_VER" >buildbot-upload-version.txt
