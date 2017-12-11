aa76b20a28b41d1050ab05d8918bf08feaa67703
Frank Kusmiruk <frankkusmiruk@outlook.es>
2017-12-11 08:06:40 -0300

    Add config option to disable warning

--- src/main/java/ch/njol/skript/SkriptConfig.java
+++ src/main/java/ch/njol/skript/SkriptConfig.java
@@ -161,6 +161,7 @@ public abstract class SkriptConfig {
 	
 	public final static Option<Boolean> disableVariableConflictWarnings = new Option<Boolean>("disable variable conflict warnings", false);
 	public final static Option<Boolean> disableObjectCannotBeSavedWarnings = new Option<Boolean>("disable variable will not be saved warnings", false);
+	public final static Option<Boolean> disableMissingAndOrWarnings = new Option<Boolean>("disable variable missing and/or warnings", false);
 	
 	
 	public final static Option<Boolean> enableScriptCaching = new Option<Boolean>("enable script caching", false)
