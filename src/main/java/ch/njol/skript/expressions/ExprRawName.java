ee6bf0bb5797e8df7d14b00f413707c5969e10fb
bensku <bmi@iki.fi>
2018-03-21 12:13:35 +0200

    Rever changes to raw name expression, as they would conflict with aliases-rework

--- src/main/java/ch/njol/skript/expressions/ExprRawName.java
+++ src/main/java/ch/njol/skript/expressions/ExprRawName.java
@@ -17,29 +17,18 @@
  *
  * Copyright 2011-2017 Peter Güttinger and contributors
  */
-/*
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- * 
- */
 package ch.njol.skript.expressions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.bukkit.Material;
 import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffectType;
 import org.eclipse.jdt.annotation.Nullable;
 
 import ch.njol.skript.Skript;
@@ -49,18 +38,52 @@ import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.PropertyExpression;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.util.PotionEffectUtils;
 import ch.njol.util.Kleenean;
 
 @Name("Raw Name")
 @Description("Raw Minecraft material name for given item. Note that this is not guaranteed to give same results on all servers.")
 @Examples("raw name of tool of player")
-@Since("unknown (2.2-Fixes-v10), 2.2-dev35")
-public class ExprRawName extends PropertyExpression<ItemType, String> {
+@Since("unknown (2.2)")
+public class ExprRawName extends SimpleExpression<String> {
 	
 	static {
-		register(ExprRawName.class, String.class, "(raw|minecraft|vanilla) name[s]", "itemtypes");
+		Skript.registerExpression(ExprRawName.class, String.class, ExpressionType.SIMPLE, "(raw|minecraft|vanilla) name of %itemtypes%");
+	}
+	
+	@Nullable
+	private Expression<ItemType> types;
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
+		this.types = (Expression<ItemType>) exprs[0];
+		return true;
+	}
+	
+	@SuppressWarnings("null")
+	@Override
+	@Nullable
+	protected String[] get(final Event e) {
+		if (types == null) return null;
+		
+		ItemType[] items = types.getAll(e);
+		List<String> names = new ArrayList<>();
+		for (int i = 0; i < items.length; i++) {
+			names.addAll(items[i].getRawNames());
+		}
+		
+		return names.toArray(new String[names.size()]);
+	}
+	
+	@Override
+	public boolean isSingle() {
+		return false;
 	}
 	
 	@Override
@@ -68,25 +91,11 @@ public class ExprRawName extends PropertyExpression<ItemType, String> {
 		return String.class;
 	}
 	
-	@SuppressWarnings({"unchecked", "null"})
-	@Override
-	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
-		setExpr((Expression<? extends ItemType>) exprs[0]);
-		return true;
-	}
-	
 	@SuppressWarnings("null")
 	@Override
-	protected String[] get(final Event event, ItemType[] source) {
-		List<String> names = new ArrayList<>();
-		for (int i = 0; i < source.length; i++) {
-			names.addAll(source[i].getRawNames());
-		}
-		return names.toArray(new String[names.size()]);
-	}
-
-	@Override
-	public String toString(final @Nullable Event event, final boolean debug) {
-		return "the raw name " + (getExpr().isDefault() ? "" : " of " + getExpr().toString(event, debug));
+	public String toString(final @Nullable Event e, final boolean debug) {
+		String[] strs = get(e);
+		if (strs == null) return "";
+		return Arrays.toString(strs);
 	}
 }
