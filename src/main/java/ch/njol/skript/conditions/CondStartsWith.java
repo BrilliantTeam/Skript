dc6876ce2109ae31d2fd66f23ec1898e6630798b
Snow-Pyon <frankkusmiruk@outlook.es>
2018-05-02 23:16:38 -0300

    Add starts/ends with condition

--- src/main/java/ch/njol/skript/conditions/CondStartsWith.java
+++ src/main/java/ch/njol/skript/conditions/CondStartsWith.java
@@ -1,15 +1,43 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Copyright 2011-2017 Peter Güttinger and contributors
+ */
 package ch.njol.skript.conditions;
 
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Condition;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Checker;
 import ch.njol.util.Kleenean;
 
+@Name("Starts/Ends With")
+@Description("Checks if a text starts or ends with another")
+@Examples({"if the argument starts with \"test\":",
+		"	send \"Stop!\""})
+@Since("INSERT VERSION")
 public class CondStartsWith extends Condition {
 
 	static {
