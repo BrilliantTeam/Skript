e6fd436d65b4f93a36c83ebae06e60ef6089c653
Nicofisi <m.lomiak@gmail.com>
2018-07-29 19:58:33 +0200

    Add an optional element to the syntax of ExprPlayerWeather

--- src/main/java/ch/njol/skript/expressions/ExprPlayerWeather.java
+++ src/main/java/ch/njol/skript/expressions/ExprPlayerWeather.java
@@ -45,7 +45,7 @@ import org.eclipse.jdt.annotation.Nullable;
 public class ExprPlayerWeather extends SimplePropertyExpression<Player, WeatherType> {
 
 	static {
-		register(ExprPlayerWeather.class, WeatherType.class, "weather", "players");
+		register(ExprPlayerWeather.class, WeatherType.class, "[(client|custom)] weather", "players");
 	}
 
 	@Override
