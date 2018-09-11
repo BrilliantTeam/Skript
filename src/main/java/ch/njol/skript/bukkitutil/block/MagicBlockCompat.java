c7d1efdd5307ef8e402cbc33a989cbb2ad88cbae
bensku <bmi@iki.fi>
2018-09-11 20:50:40 +0300

    1.12 support: patch block values together

--- src/main/java/ch/njol/skript/bukkitutil/block/MagicBlockCompat.java
+++ src/main/java/ch/njol/skript/bukkitutil/block/MagicBlockCompat.java
@@ -69,30 +69,16 @@ public class MagicBlockCompat implements BlockCompat {
 	private class MagicBlockValues extends BlockValues {
 
 		private Material id;
-		private byte data;
+		short data;
 
 		public MagicBlockValues(BlockState block) {
 			this.id = block.getType();
 			this.data = block.getRawData(); // Some black magic here, please look away...
 		}
 		
-		public MagicBlockValues(ItemStack stack) {
-			this.id = stack.getType();
-			this.data = stack.getData().getData(); // And terrible hack again
-		}
-		
-		public MagicBlockValues(Material id, byte data) {
+		public MagicBlockValues(Material id, short data) {
 			this.id = id;
 			this.data = data;
-		}
-
-		public void setBlock(Block block, boolean applyPhysics) {
-			block.setType(id);
-			try {
-				setDataMethod.invokeExact(block, data);
-			} catch (Throwable e) {
-				Skript.exception(e);
-			}
 		}
 
 		@Override
@@ -109,6 +95,26 @@ public class MagicBlockCompat implements BlockCompat {
 			// byte -> int widening seems to be a bit weird in Java
 			return (id.ordinal() << 8) | (data & 0xff);
 		}
+	}
+	
+	private static class MagicBlockSetter implements BlockSetter {
+
+		public MagicBlockSetter() {}
+
+		@Override
+		public void setBlock(Block block, Material type, @Nullable BlockValues values, int flags) {
+			block.setType(type);
+			
+			if (values != null) {
+				MagicBlockValues ourValues = (MagicBlockValues) values;
+				try {
+					setDataMethod.invokeExact(block, (byte) ourValues.data);
+				} catch (Throwable e) {
+					Skript.exception(e);
+				}
+			}
+		}
+		
 		
 	}
 
@@ -129,172 +135,11 @@ public class MagicBlockCompat implements BlockCompat {
 		}
 		return state;
 	}
-
-	private Map<String,String> parseState(String state) {
-		Map<String,String> parsed = new HashMap<>();
-		
-		int comma;
-		int pos = 0;
-		while (pos != -1) { // Loop until we don't have more key=value pairs
-			comma = state.indexOf(',', pos); // Find where next key starts
-			
-			// Get key=value as string
-			String pair;
-			if (comma == -1) {
-				pair = state.substring(pos);
-				pos = -1;
-			} else {
-				pair = state.substring(pos, comma);
-				pos = comma + 1;
-			}
-			
-			// Split pair to parts, add them to map
-			String[] parts = pair.split("=");
-			parsed.put(parts[0], parts[1]);
-		}
-		
-		return parsed;
-	}
 	
 	@Nullable
 	@Override
 	public BlockValues createBlockValues(Material type, Map<String, String> states) {
-//		Map<String, String> states = parseState(state);
-		int data = 0;
-		
-//		for (Map.Entry<String, String> entry : states.entrySet()) {
-//			String value = entry.getValue();
-//			switch (entry.getKey()) {
-//				case "damage": // Anvil
-//					int damage = Integer.parseInt(value);
-//					if (damage == 1) {
-//						data |= 4;
-//					} else if (damage == 2) {
-//						data |= 8;
-//					}
-//					break;
-//				case "facing": // 4 to 8 possible rotations
-//					int facing = 0;
-//					
-//					switch (type) {
-//						case ANVIL: // 4 directions
-//						case BED_BLOCK:
-//							switch (value) {
-//								case "south":
-//									// No changes
-//									break;
-//								case "west":
-//									facing = 1;
-//									break;
-//								case "north":
-//									facing = 2;
-//									break;
-//								case "east":
-//									facing = 3;
-//									break;
-//							}
-//							
-//							data |= facing;
-//							break;
-//						case WOOD_BUTTON: // 6 directions
-//						case STONE_BUTTON:
-//							switch (value) {
-//								case "bottom":
-//									// No changes
-//									break;
-//								case "east":
-//									facing = 1;
-//									break;
-//								case "west":
-//									facing = 2;
-//									break;
-//								case "south":
-//									facing = 3;
-//									break;
-//								case "north":
-//									facing = 4;
-//									break;
-//								case "up":
-//									facing = 5;
-//									break;
-//							}
-//							
-//							data |= facing;
-//							break;
-//						//$CASES-OMITTED$
-//						default:
-//							break;
-//					}
-//					break;
-//				case "rotation": // 16 possible rotations
-//					int rotation = Integer.parseInt(value);
-//					data |= rotation; // No way going to write lookup table for THAT
-//					break;
-//				case "occupied":
-//					if (value.equals("true")) {
-//						data |= 4;
-//					}
-//					break;
-//				case "part": // Beds, doors, some plants
-//					switch (type) {
-//						case BED: // Bed foot or head
-//							if (value.equals("head")) {
-//								data |= 8;
-//							}
-//							break;
-//						//$CASES-OMITTED$
-//						default:
-//							break;
-//					}
-//					break;
-//				case "age": // Age of plants or frosted ice
-//					int age = Integer.parseInt(value);
-//					data |= age;
-//					break;
-//				case "axis": // 3 possible rotations
-//					// TODO investigate/reverse-engineer
-//					break;
-//				case "bites": // Cake bites eaten
-//					int bites = Integer.parseInt(value);
-//					data |= bites;
-//					break;
-//				case "level": // Lava/water level, includes cauldron
-//					int level = Integer.parseInt(value);
-//					data |= level;
-//					break;
-//				case "north": // Block rotations in some cases
-//					// TODO investigate/reverse engineer
-//					break;
-//				case "south":
-//					break;
-//				case "east":
-//					break;
-//				case "west":
-//					break;
-//				case "up":
-//					break;
-//				case "down":
-//					break;
-//				case "conditional": // Command block mode
-//					// TODO
-//					break;
-//				case "snowy": // Snow on dirt
-//					break;
-//				case "in_wall": // Fence gate is lowered a bit
-//					break;
-//				case "open": // Fence gates, doors
-//					break;
-//				case "powered": // Block gets redstone power
-//					break;
-//				case "check_decay": // Leaf decay check
-//					break;
-//				case "decayable": // Leaf decay possibility
-//					break;
-//			}
-//		}
-		// FIXME re-enable or figure out something better
-		
-		return new MagicBlockValues(type, (byte) data);
+		return new MagicBlockValues(type, (byte) 0); // TODO maybe support block states?
 	}
 
 	@Override
@@ -308,14 +153,19 @@ public class MagicBlockCompat implements BlockCompat {
 		return type == Material.WATER || type == Material.LAVA;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
+	@Nullable
 	public BlockValues getBlockValues(ItemStack stack) {
-		throw new UnsupportedOperationException("not yet implemented");
+		short data = stack.getDurability();
+		if (data != 0)
+			return new MagicBlockValues(stack.getType(), data);
+		return null;
 	}
 
 	@Override
 	public BlockSetter getSetter() {
-		throw new UnsupportedOperationException("not yet implemented");
+		return new MagicBlockSetter();
 	}
 	
 }
