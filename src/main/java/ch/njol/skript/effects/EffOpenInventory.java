12b867d363047b202e10ec3bab522a0182e1dbb8
BuildTools <shanebolenback@me.com>
2018-11-23 01:54:41 -0800

    Removed furnace, brewing, enchanting and beacon

--- src/main/java/ch/njol/skript/effects/EffOpenInventory.java
+++ src/main/java/ch/njol/skript/effects/EffOpenInventory.java
@@ -44,14 +44,14 @@ import ch.njol.util.Kleenean;
 		"Please note that currently 'show' and 'open' have the same effect, but 'show' will eventually show an unmodifiable view of the inventory in the future."})
 @Examples({"show the victim's inventory to the player",
 		"open the player's inventory for the player"})
-@Since("2.0, 2.1.1 (closing), 2.2-Fixes-V10 (anvil), {INSERT VERSION} (hopper, dropper, dispenser, furnace, brewing, beacon, enchanting table")
+@Since("2.0, 2.1.1 (closing), 2.2-Fixes-V10 (anvil), {INSERT VERSION} (hopper, dropper, dispenser")
 public class EffOpenInventory extends Effect {
 	
-	private final static int WORKBENCH = 0, CHEST = 1, ANVIL = 2, HOPPER = 3, DROPPER = 4, DISPENSER = 5, FURNACE = 6, BREWING = 7, BEACON = 8, ENCHANTTABLE = 9;
+	private final static int WORKBENCH = 0, CHEST = 1, ANVIL = 2, HOPPER = 3, DROPPER = 4, DISPENSER = 5;
 	
 	static {
 		Skript.registerEffect(EffOpenInventory.class,
-				"(0¦open|1¦show) ((2¦(crafting [table]|workbench)|3¦chest|4¦anvil|5¦hopper|6¦dropper|7¦dispenser|8¦furnace|9¦brewing|10¦beacon|11¦enchant(ing|ment) table) (view|window|inventory|)|%-inventory%) (to|for) %players%",
+				"(0¦open|1¦show) ((2¦(crafting [table]|workbench)|3¦chest|4¦anvil|5¦hopper|6¦dropper|7¦dispenser) (view|window|inventory|)|%-inventory%) (to|for) %players%",
 				"close [the] inventory [view] (to|of|for) %players%", "close %players%'[s] inventory [view]");
 	}
 	
@@ -68,19 +68,7 @@ public class EffOpenInventory extends Effect {
 	@Override
 	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
 		int openFlag = 0;
-		if(parseResult.mark >= 11) {
-			openFlag = parseResult.mark ^ 11;
-			invType = ENCHANTTABLE;
-		} else if(parseResult.mark >= 10) {
-			openFlag = parseResult.mark ^ 10;
-			invType = BEACON;
-		} else if(parseResult.mark >= 9) {
-			openFlag = parseResult.mark ^ 9;
-			invType = BREWING;
-		} else if(parseResult.mark >= 8) {
-			openFlag = parseResult.mark ^ 8;
-			invType = FURNACE;
-		} else if(parseResult.mark >= 7) {
+		if(parseResult.mark >= 7) {
 			openFlag = parseResult.mark ^ 7;
 			invType = DISPENSER;
 		} else if(parseResult.mark >= 6) {
@@ -145,19 +133,7 @@ public class EffOpenInventory extends Effect {
 							break;
 						case DISPENSER:
 							p.openInventory(Bukkit.createInventory(p, InventoryType.DISPENSER));
-							break;
-						case FURNACE:
-							p.openInventory(Bukkit.createInventory(p, InventoryType.FURNACE));
-							break;
-						case BREWING:
-							p.openInventory(Bukkit.createInventory(p, InventoryType.BREWING));
-							break;
-						case BEACON:
-							p.openInventory(Bukkit.createInventory(p, InventoryType.BEACON));
-							break;
-						case ENCHANTTABLE:
-							p.openInventory(Bukkit.createInventory(p, InventoryType.ENCHANTING));
-						
+					
 					}
 				} else
 					p.closeInventory();
