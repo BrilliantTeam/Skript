4c50b6f1cf37cbce4ee38f60eb8928e00f040196
bensku <bmi@iki.fi>
2018-12-06 13:26:42 +0200

    Restory accidentally deleted Messages

--- src/main/java/ch/njol/skript/SkriptUpdater.java
+++ src/main/java/ch/njol/skript/SkriptUpdater.java
@@ -26,6 +26,8 @@ import java.util.concurrent.CompletableFuture;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import ch.njol.skript.localization.FormattedMessage;
+import ch.njol.skript.localization.Message;
 import ch.njol.skript.update.ReleaseChannel;
 import ch.njol.skript.update.ReleaseManifest;
 import ch.njol.skript.update.UpdateChecker;
@@ -37,6 +39,20 @@ import ch.njol.skript.update.UpdaterState;
  * Skript's updater checker.
  */
 public class SkriptUpdater extends Updater {
+	
+	public final static Message m_not_started = new Message("updater.not started");
+	public final static Message m_checking = new Message("updater.checking");
+	public final static Message m_check_in_progress = new Message("updater.check in progress");
+	public final static FormattedMessage m_check_error = new FormattedMessage("updater.check error", error);
+	public final static Message m_running_latest_version = new Message("updater.running latest version");
+	public final static Message m_running_latest_version_beta = new Message("updater.running latest version (beta)");
+	public final static FormattedMessage m_update_available = new FormattedMessage("updater.update available", latest, Skript.getVersion());
+	public final static FormattedMessage m_downloading = new FormattedMessage("updater.downloading", latest);
+	public final static Message m_download_in_progress = new Message("updater.download in progress");
+	public final static FormattedMessage m_download_error = new FormattedMessage("updater.download error", error);
+	public final static FormattedMessage m_downloaded = new FormattedMessage("updater.downloaded", latest);
+	public final static Message m_internal_error = new Message("updater.internal error");
+	public final static Message m_custom_version = new Message("updater.custom version");
 	
 	SkriptUpdater() {
 		super(loadManifest());
