85a4b7ff4b514797b26093b7dbf1d9634df9821f
bensku <bmi@iki.fi>
2019-01-26 13:49:54 +0200

    Fix insert version

--- src/main/java/ch/njol/skript/effects/EffOpenInventory.java
+++ src/main/java/ch/njol/skript/effects/EffOpenInventory.java
@@ -38,15 +38,12 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 
-/**
- * @author Peter Güttinger
- */
 @Name("Open/Close Inventory")
 @Description({"Opens an inventory to a player. The player can then access and modify the inventory as if it was a chest that he just opened.",
 		"Please note that currently 'show' and 'open' have the same effect, but 'show' will eventually show an unmodifiable view of the inventory in the future."})
 @Examples({"show the victim's inventory to the player",
 		"open the player's inventory for the player"})
-@Since("2.0, 2.1.1 (closing), 2.2-Fixes-V10 (anvil), {INSERT VERSION} (hopper, dropper, dispenser")
+@Since("2.0, 2.1.1 (closing), 2.2-Fixes-V10 (anvil), INSERT VERSION (hopper, dropper, dispenser")
 public class EffOpenInventory extends Effect {
 	
 	private final static int WORKBENCH = 0, CHEST = 1, ANVIL = 2, HOPPER = 3, DROPPER = 4, DISPENSER = 5;
