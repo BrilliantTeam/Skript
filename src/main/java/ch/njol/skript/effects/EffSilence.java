830942709625b1ccc43cf14ef4f7edf90c870bb8
OfficialDonut <donut42915@gmail.com>
2019-10-02 11:32:41 -0400

    Merge EffSilence and EffUnsilence

--- src/main/java/ch/njol/skript/effects/EffSilence.java
+++ src/main/java/ch/njol/skript/effects/EffSilence.java
@@ -34,34 +34,40 @@ import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
 
 @Name("Silence Entity")
-@Description("Makes an entity silent.")
+@Description("Makes an entity silent/unsilent.")
 @Examples("make target entity silent")
 @Since("INSERT VERSION")
 public class EffSilence extends Effect {
 	
 	static {
-		Skript.registerEffect(EffSilence.class, "silence %entities%", "make %entities% silent");
+		Skript.registerEffect(EffSilence.class,
+			"silence %entities%",
+			"unsilence %entities%",
+			"make %entities% silent",
+			"make %entities% (not silent|unsilent)");
 	}
 	
 	@SuppressWarnings("null")
 	private Expression<Entity> entities;
+	private boolean silence;
 	
 	@SuppressWarnings({"unchecked", "null"})
 	@Override
 	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final SkriptParser.ParseResult parser) {
 		entities = (Expression<Entity>) exprs[0];
+		silence = matchedPattern % 2 == 0;
 		return true;
 	}
 	
 	@Override
 	protected void execute(final Event e) {
 		for (Entity entity : entities.getArray(e)) {
-			entity.setSilent(true);
+			entity.setSilent(silence);
 		}
 	}
 	
 	@Override
 	public String toString(final @Nullable Event e, final boolean debug) {
-		return "silence" + entities.toString(e, debug);
+		return (silence ? "silence" : "unsilence") + entities.toString(e, debug);
 	}
 }
