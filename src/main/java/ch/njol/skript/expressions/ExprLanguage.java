7b783fa795219640b837f4b66d5f61297e5d8333
bensku <bmi@iki.fi>
2019-12-22 15:29:14 +0200

    Compile against Paper 1.15 API

--- src/main/java/ch/njol/skript/expressions/ExprLanguage.java
+++ src/main/java/ch/njol/skript/expressions/ExprLanguage.java
@@ -20,6 +20,11 @@
 package ch.njol.skript.expressions;
 
 import org.eclipse.jdt.annotation.Nullable;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 import org.bukkit.entity.Player;
 import ch.njol.skript.Skript;
 import ch.njol.skript.doc.Description;
@@ -36,16 +41,33 @@ import ch.njol.skript.expressions.base.SimplePropertyExpression;
 public class ExprLanguage extends SimplePropertyExpression<Player, String> {
 
 	private static final boolean USE_DEPRECATED_METHOD = !Skript.methodExists(Player.class, "getLocale");
+	
+	@Nullable
+	private static final MethodHandle getLocaleMethod;
 
 	static {
 		register(ExprLanguage.class, String.class, "[([currently] selected|current)] [game] (language|locale) [setting]", "players");
+		
+		MethodHandle handle;
+		try {
+			handle = MethodHandles.lookup().findVirtual(Player.Spigot.class, "getLocale", MethodType.methodType(String.class));
+		} catch (NoSuchMethodException | IllegalAccessException e) {
+			handle = null;
+		}
+		getLocaleMethod = handle;
 	}
 
 	@Override
 	@Nullable
 	public String convert(Player p) {
 		if (USE_DEPRECATED_METHOD) {
-			return p.spigot().getLocale();
+			assert getLocaleMethod != null;
+			try {
+				return (String) getLocaleMethod.invoke();
+			} catch (Throwable e) {
+				Skript.exception(e);
+				return null;
+			}
 		} else {
 			return p.getLocale();
 		}
