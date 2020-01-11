ed8a593b2ca365d5122a2d9c7c25c04a6ebf705e
bensku <bmi@iki.fi>
2020-01-11 17:07:27 +0200

    Add sponge absorb event (#1655)

--- src/main/java/ch/njol/skript/events/SimpleEvents.java
+++ src/main/java/ch/njol/skript/events/SimpleEvents.java
@@ -30,6 +30,7 @@ import org.bukkit.event.block.BlockRedstoneEvent;
 import org.bukkit.event.block.BlockSpreadEvent;
 import org.bukkit.event.block.LeavesDecayEvent;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.block.SpongeAbsorbEvent;
 import org.bukkit.event.entity.AreaEffectCloudApplyEvent;
 import org.bukkit.event.entity.CreeperPowerEvent;
 import org.bukkit.event.entity.EntityBreakDoorEvent;
@@ -526,6 +527,15 @@ public class SimpleEvents {
 				.examples("on armor change:",
 					"	send \"You equipped %event-item%!\"")
 				.since("INSERT VERSION");
+		}
+		if (Skript.classExists("org.bukkit.event.block.SpongeAbsorbEvent")) {
+			Skript.registerEvent("Sponge Absorb", SimpleEvent.class, SpongeAbsorbEvent.class, "sponge absorb")
+					.description("Called when a sponge absorbs blocks.")
+					.requiredPlugins("Minecraft 1.13 or newer")
+					.examples("on sponge absorb:",
+							"\tloop absorbed blocks:",
+							"\t\tbroadcast \"%loop-block% was absorbed by a sponge\"!")
+					.since("INSERT VERSION");
 		}
 	}
 }
