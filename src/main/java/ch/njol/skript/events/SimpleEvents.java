44a9326f6c906381d05e80c815179a5d9f940045
Shane Bee <shanebolenback@me.com>
2020-01-11 07:20:38 -0800

    Player damage item event (PR #2712)
    * SimpleEvents - add an event for player damaging their tool
    
    * item damage event - changed to include item
    
    * SimpleEvents - changes requested by Snow
    
    * SimpleEvents - remove tool from syntax for item damage event
    
    * SimpleEvents - update description on item damage event
    
    * SimpleEvents - change description of item damage event

--- src/main/java/ch/njol/skript/events/SimpleEvents.java
+++ src/main/java/ch/njol/skript/events/SimpleEvents.java
@@ -61,6 +61,7 @@ import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerItemBreakEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
@@ -265,6 +266,11 @@ public class SimpleEvents {
 					.examples("on tool break:")
 					.since("2.1.1");
 		}
+		Skript.registerEvent("Item Damage", SimpleEvent.class, PlayerItemDamageEvent.class, "item damag(e|ing)")
+				.description("This event is called when an item is damaged such as the player using their tool or armour taking damage.")
+				.examples("on tool damage:",
+						"\tcancel event")
+				.since("INSERT VERSION");
 		Skript.registerEvent("Tool Change", SimpleEvent.class, PlayerItemHeldEvent.class, "[player['s]] (tool|item held|held item) chang(e|ing)")
 				.description("Called whenever a player changes their held item by selecting a different slot (e.g. the keys 1-9 or the mouse wheel), <i>not</i> by dropping or replacing the item in the current slot.")
 				.examples("on player's held item change:")
