ddf86e53a95e1ee19c0dd992093ade845d63455b
APickledWalrus <apickledwalrus@gmail.com>
2020-02-03 08:14:39 -0500

    Add support for creating fiery explosions (PR #2783)

--- src/main/java/ch/njol/skript/effects/EffExplosion.java
+++ src/main/java/ch/njol/skript/effects/EffExplosion.java
@@ -46,35 +46,34 @@ import ch.njol.util.Kleenean;
 		"create an explosion of force 0 at the victim"})
 @Since("1.0")
 public class EffExplosion extends Effect {
-	
+
 	static {
 		Skript.registerEffect(EffExplosion.class,
-				"[(create|make)] [an] explosion (of|with) (force|strength|power) %number% [%directions% %locations%]",
+				"[(create|make)] [an] explosion (of|with) (force|strength|power) %number% [%directions% %locations%] [(1¦with fire)]",
 				"[(create|make)] [a] safe explosion (of|with) (force|strength|power) %number% [%directions% %locations%]",
 				"[(create|make)] [a] fake explosion [%directions% %locations%]",
 				"[(create|make)] [an] explosion[ ]effect [%directions% %locations%]");
 	}
-	
+
 	@Nullable
 	private Expression<Number> force;
 	@SuppressWarnings("null")
 	private Expression<Location> locations;
-	
+
 	private boolean blockDamage;
-	
+
+	private boolean setFire;
+
 	@SuppressWarnings({"unchecked", "null"})
 	@Override
 	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parser) {
 		force = matchedPattern <= 1 ? (Expression<Number>) exprs[0] : null;
 		blockDamage = matchedPattern != 1;
-		if (!blockDamage && !Skript.isRunningMinecraft(1, 4, 5)) {
-			Skript.error("Explosions which do not destroy blocks are only available in Bukkit 1.4.5+");
-			return false;
-		}
+		setFire = parser.mark == 1;
 		locations = Direction.combine((Expression<? extends Direction>) exprs[exprs.length - 2], (Expression<? extends Location>) exprs[exprs.length - 1]);
 		return true;
 	}
-	
+
 	@Override
 	public void execute(final Event e) {
 		final Number power = force != null ? force.getSingle(e) : 0;
@@ -84,10 +83,10 @@ public class EffExplosion extends Effect {
 			if (!blockDamage)
 				l.getWorld().createExplosion(l.getX(), l.getY(), l.getZ(), power.floatValue(), false, false);
 			else
-				l.getWorld().createExplosion(l, power.floatValue());
+				l.getWorld().createExplosion(l, power.floatValue(), setFire);
 		}
 	}
-	
+
 	@Override
 	public String toString(final @Nullable Event e, final boolean debug) {
 		if (force != null)
@@ -95,5 +94,5 @@ public class EffExplosion extends Effect {
 		else
 			return "create explosion effect " + locations.toString(e, debug);
 	}
-	
+
 }
