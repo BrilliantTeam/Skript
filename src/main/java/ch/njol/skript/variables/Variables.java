132a5598d44be2b25e5d3afee6c5ff508679e1c6
TPGamesNL <29547183+TPGamesNL@users.noreply.github.com>
2021-05-10 10:46:06 +0200

    Concurrency updates (#3817)
    A few changes to AsyncEffect:
    
    - Update javadoc with a notice to ScriptLoader.hasDelayBefore = Kleenean.TRUE (otherwise the parser will think the event isn't delayed while it is).
    - Make local variables available in the effect itself, not just after it.
    - Cleaned up the code a bit.
    - Also updated EffLoadServerIcon to not throw an exception when its argument evaluated to null, and to set the hasDelayBefore field appropriately.

--- src/main/java/ch/njol/skript/variables/Variables.java
+++ src/main/java/ch/njol/skript/variables/Variables.java
@@ -28,6 +28,7 @@ import java.util.Map.Entry;
 import java.util.Queue;
 import java.util.TreeMap;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.locks.Lock;
@@ -244,7 +245,7 @@ public abstract class Variables {
 	/**
 	 * Not to be accessed outside of Bukkit's main thread!
 	 */
-	private final static Map<Event, VariablesMap> localVariables = new HashMap<>();
+	private final static Map<Event, VariablesMap> localVariables = new ConcurrentHashMap<>();
 	
 	/**
 	 * Remember to lock with {@link #getReadLock()} and to not make any changes!
@@ -348,9 +349,7 @@ public abstract class Variables {
 		}
 		if (local) {
 			assert e != null : n;
-			VariablesMap map = localVariables.get(e);
-			if (map == null)
-				localVariables.put(e, map = new VariablesMap());
+			VariablesMap map = localVariables.computeIfAbsent(e, event -> new VariablesMap());
 			map.setVariable(n, value);
 		} else {
 			setVariable(n, value);
