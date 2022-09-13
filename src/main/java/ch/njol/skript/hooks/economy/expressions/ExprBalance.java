f1e7db5dee38717c1968510c19d4fac20d6b8abb
Ayham Al Ali <alali_ayham@yahoo.com>
2022-09-14 01:31:23 +0300

    ⚒️ Improve ExprBalance (#4640)
    * ⚒️ Improve ExprBalance

--- src/main/java/ch/njol/skript/hooks/economy/expressions/ExprBalance.java
+++ src/main/java/ch/njol/skript/hooks/economy/expressions/ExprBalance.java
@@ -32,28 +32,27 @@ import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.hooks.VaultHook;
 import ch.njol.skript.hooks.economy.classes.Money;
 
-/**
- * @author Peter Güttinger
- */
 @Name("Money")
-@Description("How much virtual money a player has (can be changed). This expression requires Vault and a compatible economy plugin to be installed.")
-@Examples({"message \"You have %player's money%\" # the currency name will be added automatically",
-		"remove 20$ from the player's balance # replace '$' by whatever currency you use",
-		"add 200 to the player's account # or omit the currency alltogether"})
-@Since("2.0, 2.5 (offline player support)")
-@RequiredPlugins({"Vault", "a permission plugin that supports Vault"})
+@Description("How much virtual money a player has (can be changed).")
+@Examples({
+	"message \"You have %player's money%\" # the currency name will be added automatically",
+	"remove 20$ from the player's balance # replace '$' by whatever currency you use",
+	"add 200 to the player's account # or omit the currency altogether"
+})
+@Since("2.0, 2.5 (offline players)")
+@RequiredPlugins({"Vault", "an economy plugin that supports Vault"})
 public class ExprBalance extends SimplePropertyExpression<OfflinePlayer, Money> {
+
 	static {
 		register(ExprBalance.class, Money.class, "(money|balance|[bank] account)", "offlineplayers");
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Override
-	public Money convert(final OfflinePlayer p) {
+	public Money convert(OfflinePlayer player) {
 		try {
-			return new Money(VaultHook.economy.getBalance(p));
-		}catch(Exception e){
-			return new Money(VaultHook.economy.getBalance(p.getName()));
+			return new Money(VaultHook.economy.getBalance(player));
+		} catch (Exception ex) {
+			return new Money(VaultHook.economy.getBalance(player.getName()));
 		}
 	}
 	
@@ -69,44 +68,37 @@ public class ExprBalance extends SimplePropertyExpression<OfflinePlayer, Money> 
 	
 	@Override
 	@Nullable
-	public Class<?>[] acceptChange(final ChangeMode mode) {
+	public Class<?>[] acceptChange(ChangeMode mode) {
 		if (mode == ChangeMode.REMOVE_ALL)
 			return null;
 		return new Class[] {Money.class, Number.class};
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Override
-	public void change(final Event e, final @Nullable Object[] delta, final ChangeMode mode) throws UnsupportedOperationException {
-		assert mode != ChangeMode.REMOVE_ALL;
-		
-		if (delta == null) {
-			for (final OfflinePlayer p : getExpr().getArray(e))
-				VaultHook.economy.withdrawPlayer(p.getName(), VaultHook.economy.getBalance(p.getName()));
+	public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
+		if (delta == null) { // RESET/DELETE
+			for (OfflinePlayer p : getExpr().getArray(event))
+				VaultHook.economy.withdrawPlayer(p, VaultHook.economy.getBalance(p));
 			return;
 		}
-		
-		final double m = delta[0] instanceof Number ? ((Number) delta[0]).doubleValue() : ((Money) delta[0]).getAmount();
-		for (final OfflinePlayer p : getExpr().getArray(e)) {
+
+		double money = delta[0] instanceof Number ? ((Number) delta[0]).doubleValue() : ((Money) delta[0]).getAmount();
+		for (OfflinePlayer player : getExpr().getArray(event)) {
 			switch (mode) {
 				case SET:
-					final double b = VaultHook.economy.getBalance(p.getName());
-					if (b < m) {
-						VaultHook.economy.depositPlayer(p.getName(), m - b);
-					} else if (b > m) {
-						VaultHook.economy.withdrawPlayer(p.getName(), b - m);
+					double balance = VaultHook.economy.getBalance(player);
+					if (balance < money) {
+						VaultHook.economy.depositPlayer(player, money - balance);
+					} else if (balance > money) {
+						VaultHook.economy.withdrawPlayer(player, balance - money);
 					}
 					break;
 				case ADD:
-					VaultHook.economy.depositPlayer(p.getName(), m);
+					VaultHook.economy.depositPlayer(player, money);
 					break;
 				case REMOVE:
-					VaultHook.economy.withdrawPlayer(p.getName(), m);
+					VaultHook.economy.withdrawPlayer(player, money);
 					break;
-				case DELETE:
-				case REMOVE_ALL:
-				case RESET:
-					assert false;
 			}
 		}
 	}
