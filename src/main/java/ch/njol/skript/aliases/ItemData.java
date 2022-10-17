0d49ab4d9bb988bde670c4af16e775c1b3a5dbea
Patrick Miller <apickledwalrus@gmail.com>
2022-10-17 03:21:42 -0400

    Fix ItemData Serialization (#4961)

--- src/main/java/ch/njol/skript/aliases/ItemData.java
+++ src/main/java/ch/njol/skript/aliases/ItemData.java
@@ -18,38 +18,6 @@
  */
 package ch.njol.skript.aliases;
 
-import java.io.IOException;
-import java.io.NotSerializableException;
-import java.io.StreamCorruptedException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.EntityType;
-import org.bukkit.inventory.ItemFactory;
-import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.inventory.meta.SkullMeta;
-import org.bukkit.inventory.meta.SpawnEggMeta;
-import org.bukkit.potion.PotionData;
-import org.eclipse.jdt.annotation.Nullable;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import ch.njol.util.EnumTypeAdapter;
 import ch.njol.skript.Skript;
 import ch.njol.skript.bukkitutil.BukkitUnsafe;
 import ch.njol.skript.bukkitutil.ItemUtils;
@@ -59,6 +27,31 @@ import ch.njol.skript.localization.Message;
 import ch.njol.skript.variables.Variables;
 import ch.njol.yggdrasil.Fields;
 import ch.njol.yggdrasil.YggdrasilSerializable.YggdrasilExtendedSerializable;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFactory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.potion.PotionData;
+import org.eclipse.jdt.annotation.Nullable;
+
+import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.StreamCorruptedException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class ItemData implements Cloneable, YggdrasilExtendedSerializable {
 	
@@ -79,36 +72,15 @@ public class ItemData implements Cloneable, YggdrasilExtendedSerializable {
 
 	static final ItemFactory itemFactory = Bukkit.getServer().getItemFactory();
 	
-	static final MaterialRegistry materialRegistry;
-	
 	// Load or create material registry
 	static {
-		Gson gson = new GsonBuilder().registerTypeAdapterFactory(EnumTypeAdapter.factory).serializeNulls().create();
 		Path materialsFile = Paths.get(Skript.getInstance().getDataFolder().getAbsolutePath(), "materials.json");
 		if (Files.exists(materialsFile)) {
-			String content = null;
 			try {
-				content = new String(Files.readAllBytes(materialsFile), StandardCharsets.UTF_8);
+				Files.delete(materialsFile);
 			} catch (IOException e) {
-				Skript.exception(e, "Loading material registry failed!");
+				Skript.exception(e, "Failed to remove legacy material registry file!");
 			}
-			if (content != null) {
-				String[] names = gson.fromJson(content, String[].class);
-				assert names != null;
-				materialRegistry = MaterialRegistry.load(names);
-			} else {
-				materialRegistry = new MaterialRegistry();
-			}
-		} else {
-			materialRegistry = new MaterialRegistry();
-		}
-		
-		// Always rewrite material registry, in case some updates got applied to it
-		String content = gson.toJson(materialRegistry.getMaterials());
-		try {
-			Files.write(materialsFile, content.getBytes(StandardCharsets.UTF_8));
-		} catch (IOException e) {
-			Skript.exception(e, "Saving material registry failed!");
 		}
 	}
 	
@@ -142,7 +114,7 @@ public class ItemData implements Cloneable, YggdrasilExtendedSerializable {
 	 * allow comparing it against other blocks.
 	 */
 	@Nullable
-	transient BlockValues blockValues;
+	BlockValues blockValues;
 	
 	/**
 	 * Whether this represents an item (that definitely cannot have
@@ -590,20 +562,23 @@ public class ItemData implements Cloneable, YggdrasilExtendedSerializable {
 	@Override
 	public Fields serialize() throws NotSerializableException {
 		Fields fields = new Fields(this); // ItemStack is transient, will be ignored
-		fields.putPrimitive("id", materialRegistry.getId(type));
+		fields.putPrimitive("id", type.ordinal());
 		fields.putObject("meta", stack.getItemMeta());
 		return fields;
 	}
 
+	private static final Material[] materials = Material.values();
+
 	@Override
 	public void deserialize(Fields fields) throws StreamCorruptedException, NotSerializableException {
-		this.type = materialRegistry.getMaterial(fields.getAndRemovePrimitive("id", int.class));
+		this.type = materials[fields.getAndRemovePrimitive("id", int.class)];
 		ItemMeta meta = fields.getAndRemoveObject("meta", ItemMeta.class);
-		fields.setFields(this); // Everything but ItemStack and Material
-		
+
 		// Initialize ItemStack
 		this.stack = new ItemStack(type);
 		stack.setItemMeta(meta); // Just set meta to it
+
+		fields.setFields(this); // Everything but ItemStack and Material
 	}
 	
 	/**
