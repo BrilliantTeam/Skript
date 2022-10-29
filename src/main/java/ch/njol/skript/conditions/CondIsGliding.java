ed527fd33c086ca35667b1be0822cbec50fd86d8
Jay <72931234+Ankoki@users.noreply.github.com>
2022-10-29 09:27:29 +0100

    Is Gliding Condition (#5145) (#5153)

---
+++ src/main/java/ch/njol/skript/conditions/CondIsGliding.java
@@ -0,0 +1,49 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.conditions;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import org.bukkit.entity.LivingEntity;
+
+import ch.njol.skript.conditions.base.PropertyCondition;
+
+@Name("Is Gliding")
+@Description("Checks whether a living entity is gliding.")
+@Examples("if player is gliding")
+@Since("INSERT VERSION")
+public class CondIsGliding extends PropertyCondition<LivingEntity> {
+
+	static {
+		register(CondIsGliding.class, "gliding", "livingentities");
+	}
+
+	@Override
+	public boolean check(LivingEntity entity) {
+		return entity.isGliding();
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "gliding";
+	}
+
+}
