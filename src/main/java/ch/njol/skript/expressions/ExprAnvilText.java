04d50493b4dfe9c70a666a4fff6ddd493fd0093f
Ayham Al Ali <alali_ayham@yahoo.com>
2022-12-16 04:24:11 +0300

    🚀 Add anvil input text expression (#4618)
    Co-authored-by: APickledWalrus <apickledwalrus@gmail.com>

---
+++ src/main/java/ch/njol/skript/expressions/ExprAnvilText.java
@@ -0,0 +1,71 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.Inventory;
+import org.eclipse.jdt.annotation.Nullable;
+
+@Name("Anvil Text Input")
+@Description("An expression to get the name to be applied to an item in an anvil inventory.")
+@Examples({
+		"on inventory click:",
+		"\ttype of event-inventory is anvil inventory",
+		"\tif the anvil input text of the event-inventory is \"FREE OP\":",
+		"\t\tban player"
+})
+@Since("INSERT VERSION")
+public class ExprAnvilText extends SimplePropertyExpression<Inventory, String> {
+
+	static {
+		register(ExprAnvilText.class, String.class, "anvil [inventory] (rename|text) input", "inventories");
+	}
+
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		return true;
+	}
+
+	@Override
+	@Nullable
+	public String convert(Inventory inv) {
+		if (!(inv instanceof AnvilInventory))
+			return null;
+		return ((AnvilInventory) inv).getRenameText();
+	}
+
+	@Override
+	public Class<? extends String> getReturnType() {
+		return String.class;
+	}
+
+	@Override
+	public String getPropertyName() {
+		return "anvil text input";
+	}
+	
+}
