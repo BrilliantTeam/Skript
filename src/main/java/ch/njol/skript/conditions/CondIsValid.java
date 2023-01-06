b8ffe8f5a0945a60efe50a69ca5f9a08bf2ffa80
LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
2023-01-05 18:24:07 -0700

    Add isValid for entities (#5313)

---
+++ src/main/java/ch/njol/skript/conditions/CondIsValid.java
@@ -0,0 +1,49 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.conditions;
+
+import org.bukkit.entity.Entity;
+
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+
+@Name("Is Valid")
+@Description("Checks whether an entity has died or been despawned for some other reason.")
+@Examples("if event-entity is valid")
+@Since("INSERT VERSION")
+public class CondIsValid extends PropertyCondition<Entity> {
+
+	static {
+		register(CondIsValid.class, "valid", "entities");
+	}
+
+	@Override
+	public boolean check(Entity entity) {
+		return entity.isValid();
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "valid";
+	}
+
+}
