1b9b42397c1f388177b4fee562291b4b826c9573
Shane Bee <shanebolenback@me.com>
2023-01-30 15:12:38 -0800

    FishData - add tadpole (#5412)

--- src/main/java/ch/njol/skript/entity/FishData.java
+++ src/main/java/ch/njol/skript/entity/FishData.java
@@ -18,12 +18,16 @@
  */
 package ch.njol.skript.entity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.bukkit.entity.Cod;
 import org.bukkit.entity.Fish;
 import org.bukkit.entity.PufferFish;
 import org.bukkit.entity.Salmon;
+import org.bukkit.entity.Tadpole;
 import org.bukkit.entity.TropicalFish;
 import org.eclipse.jdt.annotation.Nullable;
 
@@ -33,9 +37,14 @@ import ch.njol.skript.lang.SkriptParser.ParseResult;
 
 public class FishData extends EntityData<Fish> {
 
+	private static final boolean HAS_TADPOLE = Skript.classExists("org.bukkit.entity.Tadpole");
+	private static final List<String> PATTERNS;
+
 	static {
-		register(FishData.class, "fish", Fish.class, 0,
-				"fish", "cod", "puffer fish", "salmon", "tropical fish");
+		PATTERNS = new ArrayList<>(Arrays.asList("fish", "cod", "puffer fish", "salmon", "tropical fish"));
+		if (HAS_TADPOLE)
+			PATTERNS.add("tadpole");
+		register(FishData.class, "fish", Fish.class, 0, PATTERNS.toArray(new String[0]));
 	}
 	
 	private boolean init = true;
@@ -53,7 +62,7 @@ public class FishData extends EntityData<Fish> {
 	protected boolean init(Literal<?>[] exprs, int matchedPattern, ParseResult parseResult) {
 		if (matchedPattern == 0)
 			wildcard = true;
-		pattern = (matchedPattern == 0) ? ThreadLocalRandom.current().nextInt(1, 5) : matchedPattern;
+		pattern = (matchedPattern == 0) ? ThreadLocalRandom.current().nextInt(1, PATTERNS.size()) : matchedPattern;
 		return true;
 	}
 
@@ -62,7 +71,7 @@ public class FishData extends EntityData<Fish> {
 		int matchedPattern = getInitPattern(e);
 		if (matchedPattern == 0)
 			wildcard = true;
-		pattern = (matchedPattern == 0) ? ThreadLocalRandom.current().nextInt(1, 5) : matchedPattern;
+		pattern = (matchedPattern == 0) ? ThreadLocalRandom.current().nextInt(1, PATTERNS.size()) : matchedPattern;
 		return true;
 	}
 
@@ -90,6 +99,8 @@ public class FishData extends EntityData<Fish> {
 				return Salmon.class;
 			case 4:
 				return TropicalFish.class;
+			case 5:
+				return Tadpole.class;
 		}
 		return Fish.class;
 	}
@@ -105,31 +116,33 @@ public class FishData extends EntityData<Fish> {
 	}
 
 	@Override
-	protected boolean equals_i(EntityData<?> obj) {
-		return obj instanceof Fish ? (wildcard ? true : getPattern((Fish) obj) == pattern) : false;
+	protected boolean equals_i(EntityData<?> entityData) {
+		return entityData instanceof Fish ? (wildcard ? true : getPattern((Fish) entityData) == pattern) : false;
 	}
 
 	@Override
-	public boolean isSupertypeOf(EntityData<?> e) {
-		return e instanceof Fish ? (wildcard ? true : getPattern((Fish) e) == pattern) : false;
+	public boolean isSupertypeOf(EntityData<?> entityData) {
+		return entityData instanceof Fish ? (wildcard ? true : getPattern((Fish) entityData) == pattern) : false;
 	}
 	
-	private static int getInitPattern(@Nullable Fish f) {
-		if (f == null)
+	private static int getInitPattern(@Nullable Fish fish) {
+		if (fish == null)
 			return 0;
-		else if (f instanceof Cod)
+		else if (fish instanceof Cod)
 			return 1;
-		else if (f instanceof PufferFish)
+		else if (fish instanceof PufferFish)
 			return 2;
-		else if (f instanceof Salmon)
+		else if (fish instanceof Salmon)
 			return 3;
-		else if (f instanceof TropicalFish)
+		else if (fish instanceof TropicalFish)
 			return 4;
+		else if (HAS_TADPOLE && fish instanceof Tadpole)
+			return 5;
 		return 0;
 	}
 	
-	private int getPattern(@Nullable Fish f) {
-		int p = getInitPattern(f);
+	private int getPattern(@Nullable Fish fish) {
+		int p = getInitPattern(fish);
 		return p == 0 ? pattern : p;
 	}
 	
