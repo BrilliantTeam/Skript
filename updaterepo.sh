fce8119215a013b0ce4dcd34c7a97394203a9e38
Kiip <25848425+kiip1@users.noreply.github.com>
2023-02-16 22:36:43 +0100

    Remove buildbot (#5454)

--- updaterepo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./gradlew publish
-cd ../ItemAPI/repo #This is due to me having strange file structure...
-git add ch
-git commit -m "Update Skript to latest source version"
-git push repo master
