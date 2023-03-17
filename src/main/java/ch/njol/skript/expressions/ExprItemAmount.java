f858c4a4bf1955b4afdc108ffb5c1f7d5fdf2b07
sovdee <ethan.sovde@mail.utoronto.ca>
2023-03-17 06:53:37 -0400

    Adds Explicit ItemStack Support to ExprItemAmount, fixing issue with item amount of variables. (#5513)

--- src/main/java/ch/njol/skript/expressions/ExprItemAmount.java
+++ src/main/java/ch/njol/skript/expressions/ExprItemAmount.java
@@ -19,7 +19,6 @@
 package ch.njol.skript.expressions;
 
 import ch.njol.skript.aliases.ItemType;
-import ch.njol.skript.classes.Changer;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
@@ -29,6 +28,7 @@ import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.util.slot.Slot;
 import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
 import org.eclipse.jdt.annotation.Nullable;
 
 @Name("Item Amount")
@@ -36,64 +36,72 @@ import org.eclipse.jdt.annotation.Nullable;
 @Examples("send \"You have got %item amount of player's tool% %player's tool% in your hand!\" to player")
 @Since("2.2-dev24")
 public class ExprItemAmount extends SimplePropertyExpression<Object, Long> {
-	
-    static {
-        register(ExprItemAmount.class, Long.class, "item[[ ]stack] (amount|size|number)", "slots/itemtypes");
-    }
 
-	
+	static {
+		register(ExprItemAmount.class, Long.class, "item[[ ]stack] (amount|size|number)", "slots/itemtypes/itemstacks");
+	}
+
 	@Override
 	public Long convert(final Object item) {
-    	return (long) (item instanceof ItemType ? ((ItemType) item).getAmount() : ((Slot) item).getAmount());
+		if (item instanceof ItemType) {
+			return (long) ((ItemType) item).getAmount();
+		} else if (item instanceof Slot) {
+			return (long) ((Slot) item).getAmount();
+		} else {
+			return (long) ((ItemStack) item).getAmount();
+		}
 	}
-	
-	@Override
-    public @Nullable Class<?>[] acceptChange(Changer.ChangeMode mode) {
-        return (mode != ChangeMode.REMOVE_ALL) ? CollectionUtils.array(Number.class) : null;
-    }
 
-    @Override
-    public void change(Event event, @Nullable Object[] delta, Changer.ChangeMode mode) {
-    	int amount = delta != null ? ((Number) delta[0]).intValue() : 0;
-        switch (mode) {
-            case ADD:
-            	for (Object obj : getExpr().getArray(event))
+	@Override
+	@Nullable
+	public Class<?>[] acceptChange(ChangeMode mode) {
+		switch (mode) {
+			case SET:
+			case ADD:
+			case RESET:
+			case DELETE:
+			case REMOVE:
+				return CollectionUtils.array(Long.class);
+		}
+		return null;
+	}
+
+	@Override
+	public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
+		int amount = delta != null ? ((Number) delta[0]).intValue() : 0;
+		switch (mode) {
+			case REMOVE:
+				amount = -amount;
+				// fall through
+			case ADD:
+				for (Object obj : getExpr().getArray(event))
 					if (obj instanceof ItemType) {
 						ItemType item = ((ItemType) obj);
 						item.setAmount(item.getAmount() + amount);
-					} else {
+					} else if (obj instanceof Slot) {
 						Slot slot = ((Slot) obj);
 						slot.setAmount(slot.getAmount() + amount);
+					} else {
+						ItemStack item = ((ItemStack) obj);
+						item.setAmount(item.getAmount() + amount);
 					}
-                break;
-            case SET:
-				for (Object obj : getExpr().getArray(event))
-					if (obj instanceof ItemType)
-						((ItemType) obj).setAmount(amount);
-					else
-						((Slot) obj).setAmount(amount);
-                break;
-            case REMOVE:
+				break;
+			case RESET:
+			case DELETE:
+				amount = 1;
+				// fall through
+			case SET:
 				for (Object obj : getExpr().getArray(event))
 					if (obj instanceof ItemType) {
-						ItemType item = ((ItemType) obj);
-						item.setAmount(item.getAmount() - amount);
+						((ItemType) obj).setAmount(amount);
+					} else if (obj instanceof Slot) {
+						((Slot) obj).setAmount(amount);
 					} else {
-						Slot slot = ((Slot) obj);
-						slot.setAmount(slot.getAmount() - amount);
+						((ItemStack) obj).setAmount(amount);
 					}
-                break;
-            case REMOVE_ALL:
-            case RESET:
-			case DELETE:
-				for (Object obj : getExpr().getArray(event))
-					if (obj instanceof ItemType)
-						((ItemType) obj).setAmount(1);
-					else
-						((Slot) obj).setAmount(1);
 				break;
-        }
-    }
+		}
+	}
 
 	@Override
 	public Class<? extends Long> getReturnType() {
@@ -104,5 +112,4 @@ public class ExprItemAmount extends SimplePropertyExpression<Object, Long> {
 	protected String getPropertyName() {
 		return "item[[ ]stack] (amount|size|number)";
 	}
-
 }
