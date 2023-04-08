7b2949edbcafa53eb1ea95057161f889564cf033
sovdee <ethansovde@gmail.com>
2023-04-08 01:07:13 -0400

    Fix Comparison Mistake in CondIsWithin (#5587)

--- src/main/java/ch/njol/skript/conditions/CondIsWithin.java
+++ src/main/java/ch/njol/skript/conditions/CondIsWithin.java
@@ -135,12 +135,12 @@ public class CondIsWithin extends Condition {
 
 		// Chunks
 		if (area instanceof Chunk) {
-			return locsToCheck.check(event, (loc) -> loc.getChunk() == area, isNegated());
+			return locsToCheck.check(event, (loc) -> loc.getChunk().equals(area), isNegated());
 		}
 
 		// Worlds
 		if (area instanceof World) {
-			return locsToCheck.check(event, (loc) -> loc.getWorld() == area, isNegated());
+			return locsToCheck.check(event, (loc) -> loc.getWorld().equals(area), isNegated());
 		}
 
 		// fall-back
