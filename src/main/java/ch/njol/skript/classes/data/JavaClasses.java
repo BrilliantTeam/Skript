f2cd6a85f60e37b4d514c63b90f5936364d01fb3
LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
2023-05-01 15:06:02 -0600

    Revert "Allow literal-only string parsing" (#5654)
    Revert "Allow literal-only string parsing (#4667)"
    
    This reverts commit 1ccc6ebf8824d9984b6e21b576140279638a24a4.

--- src/main/java/ch/njol/skript/classes/data/JavaClasses.java
+++ src/main/java/ch/njol/skript/classes/data/JavaClasses.java
@@ -565,9 +565,11 @@ public class JavaClasses {
 					@Nullable
 					public String parse(String s, ParseContext context) {
 						switch (context) {
+							case DEFAULT: // in DUMMY, parsing is handled by VariableString
+								assert false;
+								return null;
 							case CONFIG: // duh
 								return s;
-							case DEFAULT:
 							case SCRIPT:
 							case EVENT:
 								if (VariableString.isQuotedCorrectly(s, true))
@@ -582,7 +584,7 @@ public class JavaClasses {
 					
 					@Override
 					public boolean canParse(ParseContext context) {
-						return true;
+						return context != ParseContext.DEFAULT;
 					}
 					
 					@Override
