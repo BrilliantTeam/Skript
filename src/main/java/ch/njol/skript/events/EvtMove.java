1f4ffeec8d9d0e69bf12a2258808132af7991494
DelayedGaming <simmz0403@gmail.com>
2023-05-24 00:56:18 +0800

    Initial (pt 1)

--- src/main/java/ch/njol/skript/events/EvtMove.java
+++ src/main/java/ch/njol/skript/events/EvtMove.java
@@ -43,7 +43,7 @@ public class EvtMove extends SkriptEvent {
 		else
 			events = CollectionUtils.array(PlayerMoveEvent.class);
 
-		Skript.registerEvent("Move", EvtMove.class, events, "%entitydata% (move|walk|step)")
+		Skript.registerEvent("Move", EvtMove.class, events, "%entitydata% (move|walk|step|:rotate)")
 			.description("Called when a player or entity moves.",
 				"NOTE: Move event will only be called when the entity/player moves position, not orientation (ie: looking around).",
 				"NOTE: These events can be performance heavy as they are called quite often.",
@@ -60,11 +60,13 @@ public class EvtMove extends SkriptEvent {
 
 	private EntityData<?> type;
 	private boolean isPlayer;
+	private boolean isRotate;
 
 	@Override
 	public boolean init(Literal<?>[] args, int matchedPattern, ParseResult parseResult) {
 		type = ((Literal<EntityData<?>>) args[0]).getSingle();
 		isPlayer = Player.class.isAssignableFrom(type.getType());
+		isRotate = parseResult.hasTag("rotate");
 
 		if (!HAS_ENTITY_MOVE && !isPlayer) {
 			Skript.error("Entity move event requires Paper 1.16.5+", ErrorQuality.SEMANTIC_ERROR);
@@ -77,10 +79,14 @@ public class EvtMove extends SkriptEvent {
 	public boolean check(Event event) {
 		if (isPlayer && event instanceof PlayerMoveEvent) {
 			PlayerMoveEvent playerEvent = (PlayerMoveEvent) event;
+			if (isRotate)
+				return rotateCheck(playerEvent.getFrom(), playerEvent.getTo());
 			return moveCheck(playerEvent.getFrom(), playerEvent.getTo());
 		} else if (HAS_ENTITY_MOVE && event instanceof EntityMoveEvent) {
 			EntityMoveEvent entityEvent = (EntityMoveEvent) event;
 			if (type.isInstance(entityEvent.getEntity())) {
+				if (isRotate)
+					return rotateCheck(entityEvent.getFrom(), entityEvent.getTo());
 				return moveCheck(entityEvent.getFrom(), entityEvent.getTo());
 			}
 		}
@@ -107,6 +113,10 @@ public class EvtMove extends SkriptEvent {
 
 	private static boolean moveCheck(Location from, Location to) {
 		return from.getX() != to.getX() || from.getY() != to.getY() || from.getZ() != to.getZ() || from.getWorld() != to.getWorld();
+	}
+
+	private static boolean rotateCheck(Location from, Location to) {
+		return !moveCheck(from, to) && from.getYaw() != to.getYaw() || from.getPitch() != to.getPitch();
 	}
 
 }
