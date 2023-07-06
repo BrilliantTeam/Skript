549ebc380b34dd544c760d7a2c23faa8973a12f7
LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
2023-07-06 11:18:08 -0600

    Fix null pointer exception in ExprBlocks when using the ExprDirection without a number defined. (#5790)
    Update ExprBlocks.java

--- src/main/java/ch/njol/skript/expressions/ExprBlocks.java
+++ src/main/java/ch/njol/skript/expressions/ExprBlocks.java
@@ -151,9 +151,11 @@ public class ExprBlocks extends SimpleExpression<Block> {
 				int distance = SkriptConfig.maxTargetBlockDistance.value();
 				if (this.direction instanceof ExprDirection) {
 					Expression<Number> numberExpression = ((ExprDirection) this.direction).amount;
-					Number number = numberExpression.getSingle(event);
-					if (numberExpression != null && number != null)
-						distance = number.intValue();
+					if (numberExpression != null) {
+						Number number = numberExpression.getSingle(event);
+						if (number != null)
+							distance = number.intValue();
+					}
 				}
 				return new BlockLineIterator(location, vector, distance);
 			} else {
