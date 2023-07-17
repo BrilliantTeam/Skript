ebc09298698c57cf33cce4ad7b79273a2ec9673c
Fusezion <fusezionstream@gmail.com>
2023-07-17 02:01:35 -0400

    Fix EffActionBar NullPointException when message is null (#5833)

--- src/main/java/ch/njol/skript/effects/EffActionBar.java
+++ src/main/java/ch/njol/skript/effects/EffActionBar.java
@@ -29,7 +29,7 @@ import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.util.chat.BungeeConverter;
 import ch.njol.skript.util.chat.ChatMessages;
 import ch.njol.util.Kleenean;
@@ -43,36 +43,35 @@ import net.md_5.bungee.api.chat.BaseComponent;
 public class EffActionBar extends Effect {
 
 	static {
-		Skript.registerEffect(EffActionBar.class, "send [the] action[ ]bar [with text] %string% to %players%");
+		Skript.registerEffect(EffActionBar.class, "send [the] action[ ]bar [with text] %string% [to %players%]");
 	}
 
-	@SuppressWarnings("null")
 	private Expression<String> message;
 
-	@SuppressWarnings("null")
 	private Expression<Player> recipients;
 
-	@SuppressWarnings({"unchecked", "null"})
 	@Override
-	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final SkriptParser.ParseResult parser) {
+	@SuppressWarnings("unchecked")
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		message = (Expression<String>) exprs[0];
 		recipients = (Expression<Player>) exprs[1];
 		return true;
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
-	protected void execute(final Event e) {
-		String msg = message.getSingle(e);
-		assert msg != null;
+	@SuppressWarnings("deprecation")
+	protected void execute(Event event) {
+		String msg = message.getSingle(event);
+		if (msg == null)
+			return;
 		BaseComponent[] components = BungeeConverter.convert(ChatMessages.parseToArray(msg));
-		for (Player player : recipients.getArray(e))
+		for (Player player : recipients.getArray(event))
 			player.spigot().sendMessage(ChatMessageType.ACTION_BAR, components);
 	}
 
 	@Override
-	public String toString(final @Nullable Event e, final boolean debug) {
-		return "send action bar " + message.toString(e, debug) + " to " + recipients.toString(e, debug);
+	public String toString(@Nullable Event event, boolean debug) {
+		return "send action bar " + message.toString(event, debug) + " to " + recipients.toString(event, debug);
 	}
 
 }
