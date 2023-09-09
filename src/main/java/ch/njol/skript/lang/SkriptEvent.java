3f0885313dad667828eb79b7184f6feadc41261c
Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>
2023-09-09 10:43:04 +0300

    🍒 [2.7] Fixes an exception from being thrown on async events (#5980)
    Fixes an exception from being thrown on async events (#5699)
    
    Co-authored-by: LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
    Co-authored-by: Moderocky <admin@moderocky.com>

--- src/main/java/ch/njol/skript/lang/SkriptEvent.java
+++ src/main/java/ch/njol/skript/lang/SkriptEvent.java
@@ -217,6 +217,13 @@ public abstract class SkriptEvent extends Structure {
 	}
 
 	/**
+	 * Override this method to allow Skript to not force synchronization.
+	 */
+	public boolean canExecuteAsynchronously() {
+		return false;
+	}
+
+	/**
 	 * Fixes patterns in event by modifying every {@link ch.njol.skript.patterns.TypePatternElement}
 	 * to be nullable.
 	 */
