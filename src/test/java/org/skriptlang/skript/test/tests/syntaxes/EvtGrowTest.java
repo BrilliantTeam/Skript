8efc550c4c782481adbab44f1c36b382509a4f1b
sovdee <10354869+sovdeeth@users.noreply.github.com>
2023-11-01 05:27:17 -0700

    Rework EvtGrow (#5639)
    * Update EvtGrow.java
    
    * Cleanup, use blockstate directly instead of blockstateblock
    
    * Update src/main/java/ch/njol/skript/events/EvtGrow.java
    
    Co-authored-by: Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>
    
    * Rework EvtGrow (simple fix, my behind!)
    
    * Update EvtGrow.java
    
    * Fix upstream paper issue and add JUnit test for 1.18+
    
    * Requested Changes
    
    * Update src/test/java/org/skriptlang/skript/test/tests/syntaxes/EvtGrowTest.java
    
    Co-authored-by: Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>

---
+++ src/test/java/org/skriptlang/skript/test/tests/syntaxes/EvtGrowTest.java
@@ -0,0 +1,70 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package org.skriptlang.skript.test.tests.syntaxes;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.test.runner.SkriptJUnitTest;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EvtGrowTest extends SkriptJUnitTest {
+
+	private Block plant, birch;
+	private static final boolean canRun = Skript.methodExists(Block.class, "applyBoneMeal", BlockFace.class);
+
+	static {
+		setShutdownDelay(1);
+	}
+
+	@Before
+	public void setBlocks() {
+		plant = setBlock(Material.WHEAT);
+		plant.getRelative(0,-1,0).setType(Material.FARMLAND);
+		birch = plant.getRelative(10,0,0);
+		birch.getRelative(0,-1,0).setType(Material.DIRT);
+		birch.setType(Material.BIRCH_SAPLING);
+	}
+
+	@Test
+	public void testGrow() {
+		if (canRun) {
+			for (int i = 0; i < 10; i++) {
+				plant.applyBoneMeal(BlockFace.UP);
+				birch.applyBoneMeal(BlockFace.UP);
+			}
+		}
+	}
+
+	@After
+	public void resetBlocks() {
+		plant.setType(Material.AIR);
+		plant.getRelative(0,-1,0).setType(Material.AIR);
+		birch.setType(Material.AIR);
+		birch.getRelative(0,-1,0).setType(Material.AIR);
+		for (int x = -4; x <5; x++)
+			for (int y = 0; y < 15; y++)
+				for (int z = -4; z < 5; z++)
+					birch.getRelative(x,y,z).setType(Material.AIR);
+	}
+
+}
