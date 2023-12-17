23a58f33bb82fb4a5960caa1ea57bb6e5a5fedd7
sovdee <10354869+sovdeeth@users.noreply.github.com>
2023-12-16 21:25:22 -0800

    Add characters between and random characters expressions (#5867)
    * Add characters between and random characters expressions, as well as tests.
    
    * Update ExprCharacters.sk
    
    * Update src/main/java/ch/njol/skript/expressions/ExprRandomCharacter.java
    
    Co-authored-by: LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>
    
    * Requested Changes
    
    * Ascii table link
    
    * Apply suggestions from code review
    
    Co-authored-by: LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
    
    * Requested Changes
    
    ---------
    
    Co-authored-by: LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
    Co-authored-by: Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>

---
+++ src/main/java/ch/njol/skript/expressions/ExprCharacters.java
@@ -0,0 +1,120 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import org.apache.commons.lang.ArrayUtils;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+@Name("Characters Between")
+@Description({
+	"All characters between two given characters, useful for generating random strings. This expression uses the Unicode numerical code " +
+	"of a character to determine which characters are between the two given characters. The <a href=\"https://www.asciitable.com/\">ASCII table linked here</a> " +
+	"shows this ordering for the first 256 characters.",
+	"If you would like only alphanumeric characters you can use the 'alphanumeric' option in the expression.",
+	"If strings of more than one character are given, only the first character of each is used."
+})
+@Examples({
+	"loop characters from \"a\" to \"f\":",
+		"\tbroadcast \"%loop-value%\"",
+	"",
+	"# 0123456789:;<=>?@ABC... ...uvwxyz",
+	"send characters between \"0\" and \"z\"",
+	"",
+	"# 0123456789ABC... ...uvwxyz",
+	"send alphanumeric characters between \"0\" and \"z\""
+})
+@Since("INSERT VERSION")
+public class ExprCharacters extends SimpleExpression<String> {
+
+	static {
+		Skript.registerExpression(ExprCharacters.class, String.class, ExpressionType.COMBINED,
+				"[(all [[of] the]|the)] [:alphanumeric] characters (between|from) %string% (and|to) %string%");
+	}
+
+	private Expression<String> start, end;
+	private boolean isAlphanumeric;
+
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		start = (Expression<String>) exprs[0];
+		end = (Expression<String>) exprs[1];
+		isAlphanumeric = parseResult.hasTag("alphanumeric");
+		return true;
+	}
+
+	@Override
+	@Nullable
+	protected String[] get(Event event) {
+		String start = this.start.getSingle(event);
+		String end = this.end.getSingle(event);
+		if (start == null || end == null)
+			return new String[0];
+
+		if (start.length() < 1 || end.length() < 1)
+			return new String[0];
+
+		char startChar = start.charAt(0);
+		char endChar = end.charAt(0);
+
+		boolean reversed = startChar > endChar;
+		char delta = reversed ? (char) -1 : (char) 1;
+
+		int min = Math.min(startChar, endChar);
+		int max = Math.max(startChar, endChar);
+
+		String[] chars = new String[max - min + 1];
+
+		for (char c = startChar; min <= c && c <= max; c += delta) {
+			if (isAlphanumeric && !Character.isLetterOrDigit(c))
+				continue;
+			chars[c - min] = String.valueOf(c);
+		}
+
+		if (reversed)
+			ArrayUtils.reverse(chars);
+
+		return chars;
+	}
+
+	@Override
+	public boolean isSingle() {
+		return false;
+	}
+
+	@Override
+	public Class<? extends String> getReturnType() {
+		return String.class;
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return "all the " + (isAlphanumeric ? "alphanumeric " : "") + "characters between " + start.toString(event, debug) + " and " + end.toString(event, debug);
+	}
+}
---
+++ src/main/java/ch/njol/skript/expressions/ExprRandomCharacter.java
@@ -0,0 +1,133 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.Literal;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Name("Random Character")
+@Description({
+	"One or more random characters between two given characters. Use 'alphanumeric' if you want only alphanumeric characters.",
+	"This expression uses the Unicode numerical code of a character to determine which characters are between the two given characters.",
+	"If strings of more than one character are given, only the first character of each is used."
+})
+@Examples({
+	"set {_captcha} to join (5 random characters between \"a\" and \"z\") with \"\"",
+	"send 3 random alphanumeric characters between \"0\" and \"z\""
+})
+@Since("INSERT VERSION")
+public class ExprRandomCharacter extends SimpleExpression<String> {
+
+	static {
+		Skript.registerExpression(ExprRandomCharacter.class, String.class, ExpressionType.COMBINED,
+				"[a|%-number%] random [:alphanumeric] character[s] (from|between) %string% (to|and) %string%");
+	}
+
+	@Nullable
+	private Expression<Number> amount;
+	private Expression<String> from, to;
+	private boolean isAlphanumeric;
+
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		amount = (Expression<Number>) exprs[0];
+		from = (Expression<String>) exprs[1];
+		to = (Expression<String>) exprs[2];
+		isAlphanumeric = parseResult.hasTag("alphanumeric");
+		return true;
+	}
+
+	@Override
+	@Nullable
+	protected String[] get(Event event) {
+		int amount = this.amount == null ? 1 : this.amount.getOptionalSingle(event).orElse(1).intValue();
+		String from = this.from.getSingle(event);
+		String to = this.to.getSingle(event);
+		if (from == null || to == null)
+			return new String[0];
+
+		if (from.length() < 1 || to.length() < 1)
+			return new String[0];
+
+		Random random = ThreadLocalRandom.current();
+		char fromChar = from.charAt(0);
+		char toChar = to.charAt(0);
+		int min = Math.min(fromChar, toChar);
+		int max = Math.max(fromChar, toChar);
+
+		String[] chars = new String[amount];
+
+		// If isAlphanumeric, we need to find the valid characters.
+		// We can't just repeat the random character generation because
+		// it's possible that there are no valid characters in the range.
+		if (isAlphanumeric) {
+			StringBuilder validChars = new StringBuilder();
+			for (int c = min; c <= max; c++) {
+				if (Character.isLetterOrDigit(c))
+					validChars.append((char) c);
+			}
+
+			if (validChars.length() == 0)
+				return new String[0];
+
+			for (int i = 0; i < amount; i++) {
+				chars[i] = String.valueOf(validChars.charAt(random.nextInt(validChars.length())));
+			}
+			return chars;
+		}
+
+		for (int i = 0; i < amount; i++) {
+			chars[i] = String.valueOf((char) (random.nextInt(max - min + 1) + min));
+		}
+
+		return chars;
+	}
+
+
+	@Override
+	public boolean isSingle() {
+		if (amount instanceof Literal)
+			return ((Literal<Number>) amount).getSingle().intValue() == 1;
+		return amount == null;
+	}
+
+	@Override
+	public Class<? extends String> getReturnType() {
+		return String.class;
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return (amount != null ? amount.toString(event, debug) : "a") + " random character between " + from.toString(event, debug) + " and " + to.toString(event, debug);
+	}
+}
