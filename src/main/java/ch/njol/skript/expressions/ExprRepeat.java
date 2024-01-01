a9e9a78b430ee168527ea7da12e858c6877d86ac
Fusezion <fusezionstream@gmail.com>
2023-12-31 21:35:14 -0500

    Repeat Expression (#5098)
    * Adds new repeat() function which utilizes java's built in <string>.repeat(<int>)
    
    * Invalid negative error
    
    * param name changes
    
    * Removed Java11 repeat
    
    * Convert to multiply() for consistency
    
    * Update branch
    
    Removed function repeat
    Added repeat expression
    
    * Add missing annotations
    
    * Mild change
    
    * Addressed requested changes
    
    * Added support for multiple strings
    
    * Add missing ending whitespace
    
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>
    
    * Requested changed
    
    Co-Authored-By: LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
    
    * Requested change
    
    Forgot about this one
    
    Co-authored-by: LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
    
    * Fix nullable count
    
    Co-authored-by: LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
    
    * Mild clean up
    
    Optional s is now required for `times`
    Added test skript
    
    Co-Authored-By: LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
    
    * Array revert
    
    * Update ExprRepeat.java
    
    Co-Authored-By: Pikachu920 <28607612+Pikachu920@users.noreply.github.com>
    
    * Revisiting after a couple months
    
    :sparkles: Updated test to be a little more informative
    :sparkles: Added an additional test for ensuring 0/null return nothing
    :sparkles: When inputting 0 or null for number we return null for strings
    :sparkles: Refactored some parts
    :sparkles: Changed `%number%` to `%integer%` zero reason not to anymore with new number converters
    
    * Apply suggestions from code review
    
    Woops thanks!
    
    Co-authored-by: Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>
    
    * Use a stream
    Personally I'm not for this entirely
    Co-authored-by: TheLimeGlass <seantgrover@gmail.com>
    
    * Use #collect instead of Java16 #toList
    
    * Use Expression#stream instead
    
    * Update test with an additional 14 or so
    For the love of god fix the list to list comparisons
    
    * Oops didn't see this
    
    * Remove duplicate var
    
    * Additional example showcasing comparison
    
    ---------
    
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>
    Co-authored-by: LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
    Co-authored-by: Pikachu920 <28607612+Pikachu920@users.noreply.github.com>
    Co-authored-by: Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>
    Co-authored-by: sovdee <10354869+sovdeeth@users.noreply.github.com>

---
+++ src/main/java/ch/njol/skript/expressions/ExprRepeat.java
@@ -0,0 +1,83 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import ch.njol.util.StringUtils;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+@Name("Repeat String")
+@Description("Repeats inputted strings a given amount of times.")
+@Examples({
+	"broadcast nl and nl repeated 200 times",
+	"broadcast \"Hello World \" repeated 5 times",
+	"if \"aa\" repeated 2 times is \"aaaa\":",
+		"\tbroadcast \"Ahhhh\" repeated 100 times"
+})
+@Since("INSERT VERSION")
+public class ExprRepeat extends SimpleExpression<String> {
+
+	static {
+		Skript.registerExpression(ExprRepeat.class, String.class, ExpressionType.COMBINED, "%strings% repeated %integer% time[s]");
+	}
+
+	private Expression<String> strings;
+	private Expression<Integer> repeatCount;
+
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		strings = (Expression<String>) exprs[0];
+		repeatCount = (Expression<Integer>) exprs[1];
+		return true;
+	}
+
+	@Override
+	protected @Nullable String[] get(Event event) {
+		int repeatCount = this.repeatCount.getOptionalSingle(event).orElse(0);
+		if (repeatCount < 1)
+			return new String[0];
+		return strings.stream(event).map(string -> StringUtils.multiply(string, repeatCount)).toArray(String[]::new);
+	}
+
+	@Override
+	public boolean isSingle() {
+		return strings.isSingle();
+	}
+
+	@Override
+	public Class<? extends String> getReturnType() {
+		return String.class;
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return strings.toString(event, debug) + " repeated " + repeatCount.toString(event, debug) + " times";
+	}
+
+}
