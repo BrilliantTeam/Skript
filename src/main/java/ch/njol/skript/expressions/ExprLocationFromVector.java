e418f6cbbeeb136ebec2254222418a2afc319610
sovdee <10354869+sovdeeth@users.noreply.github.com>
2024-01-09 09:48:12 -0800

    Force "to location" in ExprLocationFromVector (#6310)

--- src/main/java/ch/njol/skript/expressions/ExprLocationFromVector.java
+++ src/main/java/ch/njol/skript/expressions/ExprLocationFromVector.java
@@ -49,7 +49,7 @@ public class ExprLocationFromVector extends SimpleExpression<Location> {
 
 	static {
 		Skript.registerExpression(ExprLocationFromVector.class, Location.class, ExpressionType.SIMPLE,
-				"%vector% [to location] in %world%",
+				"%vector% to location in %world%",
 				"location (from|of) %vector% in %world%",
 				"%vector% [to location] in %world% with yaw %number% and pitch %number%",
 				"location (from|of) %vector% in %world% with yaw %number% and pitch %number%"
