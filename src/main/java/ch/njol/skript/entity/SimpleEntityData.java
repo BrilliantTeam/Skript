758e3f97adc9066bfd53c681c2b86a1c6d01714a
Shane Bee <shanebolenback@me.com>
2024-02-01 08:22:59 -0800

    SimpleEntityData - add new MC 1.20.3 entities (#6367)

--- src/main/java/ch/njol/skript/entity/SimpleEntityData.java
+++ src/main/java/ch/njol/skript/entity/SimpleEntityData.java
@@ -32,6 +32,7 @@ import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Bat;
 import org.bukkit.entity.Blaze;
 import org.bukkit.entity.BlockDisplay;
+import org.bukkit.entity.Breeze;
 import org.bukkit.entity.Camel;
 import org.bukkit.entity.CaveSpider;
 import org.bukkit.entity.ChestedHorse;
@@ -130,6 +131,7 @@ import org.bukkit.entity.Vindicator;
 import org.bukkit.entity.WanderingTrader;
 import org.bukkit.entity.Warden;
 import org.bukkit.entity.WaterMob;
+import org.bukkit.entity.WindCharge;
 import org.bukkit.entity.Witch;
 import org.bukkit.entity.Wither;
 import org.bukkit.entity.WitherSkeleton;
@@ -308,6 +310,11 @@ public class SimpleEntityData extends EntityData<Entity> {
 			addSimpleEntity("block display", BlockDisplay.class);
 			addSimpleEntity("interaction", Interaction.class);
 			addSuperEntity("display", Display.class);
+		}
+
+		if (Skript.isRunningMinecraft(1, 20, 3)) {
+			addSimpleEntity("breeze", Breeze.class);
+			addSimpleEntity("wind charge", WindCharge.class);
 		}
 
 		// Register zombie after Husk and Drowned to make sure both work
