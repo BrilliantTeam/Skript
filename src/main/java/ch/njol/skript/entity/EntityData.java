d864baa7c979e5499c745bd89684de0e99979117
Shane Bee <shanebolenback@me.com>
2024-03-09 12:37:53 -0800

    EntityData - check if entity can spawn (#6484)

--- src/main/java/ch/njol/skript/entity/EntityData.java
+++ src/main/java/ch/njol/skript/entity/EntityData.java
@@ -37,6 +37,7 @@ import org.bukkit.Location;
 import org.bukkit.RegionAccessor;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.eclipse.jdt.annotation.Nullable;
 
@@ -94,6 +95,7 @@ public abstract class EntityData<E extends Entity> implements SyntaxElement, Ygg
 		} catch (NoSuchMethodException | SecurityException ignored) { /* We already checked if the method exists */ }
 	}
 
+	private static final boolean HAS_ENABLED_BY_FEATURE = Skript.methodExists(EntityType.class, "isEnabledByFeature", World.class);
 	public final static String LANGUAGE_NODE = "entities";
 
 	public final static Message m_age_pattern = new Message(LANGUAGE_NODE + ".age pattern");
@@ -464,6 +466,25 @@ public abstract class EntityData<E extends Entity> implements SyntaxElement, Ygg
 	}
 
 	/**
+	 * Check if this entity type can spawn.
+	 * <p>Some entity types may be restricted by experimental datapacks.</p>
+	 *
+	 * @param world World to check if entity can spawn in
+	 * @return True if entity can spawn else false
+	 */
+	public boolean canSpawn(@Nullable World world) {
+		if (world == null)
+			return false;
+		if (HAS_ENABLED_BY_FEATURE) {
+			// Check if the entity can actually be spawned
+			// Some entity types may be restricted by experimental datapacks
+			EntityType bukkitEntityType = EntityUtils.toBukkitEntityType(this);
+            return bukkitEntityType.isEnabledByFeature(world);
+		}
+		return true;
+	}
+
+	/**
 	 * Spawn this entity data at a location.
 	 *
 	 * @param location The {@link Location} to spawn the entity at.
@@ -476,7 +497,7 @@ public abstract class EntityData<E extends Entity> implements SyntaxElement, Ygg
 
 	/**
 	 * Spawn this entity data at a location.
-	 * The consumer allows for modiciation to the entity before it actually gets spawned.
+	 * The consumer allows for modification to the entity before it actually gets spawned.
 	 * <p>
 	 * Bukkit's own {@link org.bukkit.util.Consumer} is deprecated.
 	 * Use {@link #spawn(Location, Consumer)}
@@ -494,7 +515,7 @@ public abstract class EntityData<E extends Entity> implements SyntaxElement, Ygg
 
 	/**
 	 * Spawn this entity data at a location.
-	 * The consumer allows for modiciation to the entity before it actually gets spawned.
+	 * The consumer allows for modification to the entity before it actually gets spawned.
 	 *
 	 * @param location The {@link Location} to spawn the entity at.
 	 * @param consumer A {@link Consumer} to apply the entity changes to.
@@ -503,10 +524,13 @@ public abstract class EntityData<E extends Entity> implements SyntaxElement, Ygg
 	@Nullable
 	public E spawn(Location location, @Nullable Consumer<E> consumer) {
 		assert location != null;
+		World world = location.getWorld();
+		if (!canSpawn(world))
+			return null;
 		if (consumer != null) {
 			return EntityData.spawn(location, getType(), e -> consumer.accept(this.apply(e)));
 		} else {
-			return apply(location.getWorld().spawn(location, getType()));
+			return apply(world.spawn(location, getType()));
 		}
 	}
 
