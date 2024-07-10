7fc1033230f93b533e43becf31aa1f00c03460b6
Shane Bee <shanebolenback@me.com>
2024-07-10 10:05:53 -0700

    EnchantmentType - fix toString showing namespacedkeys (#6871)

--- src/main/java/ch/njol/skript/util/EnchantmentType.java
+++ src/main/java/ch/njol/skript/util/EnchantmentType.java
@@ -19,7 +19,6 @@
 package ch.njol.skript.util;
 
 import ch.njol.skript.aliases.ItemType;
-import ch.njol.skript.bukkitutil.EnchantmentUtils;
 import ch.njol.skript.classes.ClassInfo;
 import ch.njol.skript.classes.Parser;
 import ch.njol.skript.lang.ParseContext;
@@ -92,7 +91,7 @@ public class EnchantmentType implements YggdrasilSerializable {
 
 	@Override
 	public String toString() {
-		return EnchantmentUtils.toString(type) + (level == -1 ? "" : " " + level);
+		return getEnchantmentParser().toString(type, 0) + (level == -1 ? "" : " " + level);
 	}
 
 	@SuppressWarnings("null")
@@ -107,27 +106,18 @@ public class EnchantmentType implements YggdrasilSerializable {
 	 */
 	@Nullable
 	public static EnchantmentType parse(final String s) {
-		if (ENCHANTMENT_PARSER == null) {
-			ClassInfo<Enchantment> classInfo = Classes.getExactClassInfo(Enchantment.class);
-			if (classInfo == null) {
-				throw new IllegalStateException("Enchantment ClassInfo not found");
-			}
-			ENCHANTMENT_PARSER = (Parser<Enchantment>) classInfo.getParser();
-			if (ENCHANTMENT_PARSER == null) {
-				throw new IllegalStateException("Enchantment parser not found");
-			}
-		}
+		Parser<Enchantment> enchantmentParser = getEnchantmentParser();
 		if (pattern.matcher(s).matches()) {
 			String name = s.substring(0, s.lastIndexOf(' '));
 			assert name != null;
-			final Enchantment ench = ENCHANTMENT_PARSER.parse(name, ParseContext.DEFAULT);
+			final Enchantment ench = enchantmentParser.parse(name, ParseContext.DEFAULT);
 			if (ench == null)
 				return null;
 			String level = s.substring(s.lastIndexOf(' ') + 1);
 			assert level != null;
 			return new EnchantmentType(ench, Utils.parseInt(level));
 		}
-		final Enchantment ench = ENCHANTMENT_PARSER.parse(s, ParseContext.DEFAULT);
+		final Enchantment ench = enchantmentParser.parse(s, ParseContext.DEFAULT);
 		if (ench == null)
 			return null;
 		return new EnchantmentType(ench, -1);
@@ -154,6 +144,21 @@ public class EnchantmentType implements YggdrasilSerializable {
 		if (level != other.level)
 			return false;
 		return type.equals(other.type);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static Parser<Enchantment> getEnchantmentParser() {
+		if (ENCHANTMENT_PARSER == null) {
+			ClassInfo<Enchantment> classInfo = Classes.getExactClassInfo(Enchantment.class);
+			if (classInfo == null) {
+				throw new IllegalStateException("Enchantment ClassInfo not found");
+			}
+			ENCHANTMENT_PARSER = (Parser<Enchantment>) classInfo.getParser();
+			if (ENCHANTMENT_PARSER == null) {
+				throw new IllegalStateException("Enchantment parser not found");
+			}
+		}
+		return ENCHANTMENT_PARSER;
 	}
 
 }
